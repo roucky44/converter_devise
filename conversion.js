e4bf1151d8807bdbff5d74c36490ff97c9a132f6
CJulien7 <julien.clavier@edu.devinci.fr>
2025-09-24 12:44:37 +0200

    Commit js de conversion

---
+++ conversion.js
@@ -0,0 +1,17 @@
+function convertirDevise (a, b) {
+    let resultat = a * b;
+    return resultat;
+}
+
+
+let montant = document.getElementById("montant");
+let devise = document.getElementById("devise");
+
+form.addEventListener("submit", function (e) {
+    convertirDevise(montant.value, devise.value);
+});
+
+
+
+
+
