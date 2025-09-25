4dafd713703beaf5dd874580cf9917db925ccaec
Lucas Audoubert <lucas.audoubert@edu.devinci.fr>
2025-09-25 11:59:47 +0200

    gestion erreur calcul montant

--- src/script/api.js
+++ src/script/api.js
@@ -1,40 +1,46 @@
-const key = '0fbe4279f9d4713bd23c8733'
-const base_url = 'https://v6.exchangerate-api.com/v6/'
-
+const key = "0fbe4279f9d4713bd23c8733";
+const base_url = "https://v6.exchangerate-api.com/v6/";
 
 fetch(`${base_url}${key}/codes`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    const codes = data.supported_codes
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const codes = data.supported_codes;
 
-    let options = ''
-    codes.forEach(code => {
-      options += `<option value="${code[0]}">${code[0]} - ${code[1]}</option>`
+    let options = "";
+    codes.forEach((code) => {
+      options += `<option value="${code[0]}">${code[0]} - ${code[1]}</option>`;
+    });
+
+    document.querySelector("#cible").innerHTML = options;
+    document.querySelector("#devise").innerHTML = options;
+  });
+
+async function convert() {
+  const devise = document.querySelector("#devise").value;
+  const montant = document.querySelector("#montant").value;
+  // const target = document.querySelector('#target').value
+  const result = document.querySelector("#result");
+
+  await GET`${base_url}${key}/pair/${devise}/EUR/${target}${montant}`
+    .then((response) => response.json())
+    .then((data) => {
+      result.value = data.conversion_result;
+      innerHTML;
     })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
-    document.querySelector('#cible').innerHTML = options
-    document.querySelector('#devise').innerHTML = options
+convert();
 
-  })
+const montant = document.querySelector("#montant");
 
-
-export default function convert() {  
-  const devise = document.querySelector('#devise').value
-  const montant = document.querySelector('option').value
-  const target = document.querySelector('#target').value
-  const result = document.querySelector('#result')
-  if (document.querySelector('#montant').value === '') {
-    alert('Veuillez saisir un montant')
-    return
+if ((montant.value = "")) {
+  classlist.add("input-error");
+  elseif(montant.value != 0);
+  {
+    classlist.remove("input-error");
   }
-  GET `${base_url}${key}/pair/${devise}/EUR/${target}${montant}`
-    .then(response => response.json())
-    .then(data => {
-        result.value = data.conversion_result
-            innerHTML
-    })
-    .catch(error => {
-        console.error(error)
-    })
 }
