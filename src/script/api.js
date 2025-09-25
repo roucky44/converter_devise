dce1294cdc8a597797ce48337d0a1c7df8f8e705
Lucas Audoubert <lucas.audoubert@edu.devinci.fr>
2025-09-25 09:41:48 +0200

    Call API GET pour la liste des différentes devises. (/latest/EUR)

--- src/script/api.js
+++ src/script/api.js
@@ -1,12 +1,12 @@
 const key = '0fbe4279f9d4713bd23c8733'
 const base_url = 'https://v6.exchangerate-api.com/v6/'
 
-fetch(`${base_url}${key}`)
+fetch(`${base_url}${key}/latest/EUR`)
     .then(response => response.json())
-        .then(chats => {
+        .then(devise => {
             const resultat = document.querySelector('#resultat')
-            chats.forEach(chat => {
-                resultat.innerHTML += `<img src="${chat.url}" alt="Chat">`
+            devise.forEach(devise => {
+                resultat.innerHTML += `<li> ${devise} : ${devise.conversion_rates.EUR} </li>`
             })
         })
     .catch(error => {
