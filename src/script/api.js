b9442a65632cc5e9cea8bd16669eb95cca8726c6
Lucas Audoubert <lucas.audoubert@edu.devinci.fr>
2025-09-25 10:58:35 +0200

    Fetch des supported_codes pour injection liste.

--- src/script/api.js
+++ src/script/api.js
@@ -1,14 +1,40 @@
 const key = '0fbe4279f9d4713bd23c8733'
 const base_url = 'https://v6.exchangerate-api.com/v6/'
 
-fetch(`${base_url}${key}/latest/EUR`)
+
+fetch(`${base_url}${key}/codes`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    const codes = data.supported_codes
+
+    let options = ''
+    codes.forEach(code => {
+      options += `<option value="${code[0]}">${code[0]} - ${code[1]}</option>`
+    })
+
+    document.querySelector('#cible').innerHTML = options
+    document.querySelector('#devise').innerHTML = options
+
+  })
+
+
+export default function convert() {  
+  const devise = document.querySelector('#devise').value
+  const montant = document.querySelector('option').value
+  const target = document.querySelector('#target').value
+  const result = document.querySelector('#result')
+  if (document.querySelector('#montant').value === '') {
+    alert('Veuillez saisir un montant')
+    return
+  }
+  GET `${base_url}${key}/pair/${devise}/EUR/${target}${montant}`
     .then(response => response.json())
-        .then(devise => {
-            const resultat = document.querySelector('#resultat')
-            devise.forEach(devise => {
-                resultat.innerHTML += `<li> ${devise} : ${devise.conversion_rates.EUR} </li>`
-            })
-        })
+    .then(data => {
+        result.value = data.conversion_result
+            innerHTML
+    })
     .catch(error => {
         console.error(error)
     })
+}
