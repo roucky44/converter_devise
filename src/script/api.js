917e0a0c0c977db503e66ae39e074c155e701213
Lucas Audoubert <lucas.audoubert@edu.devinci.fr>
2025-09-26 09:36:08 +0200

    Update apis.js query target value

--- src/script/api.js
+++ src/script/api.js
@@ -1,38 +1,37 @@
 const key = "0fbe4279f9d4713bd23c8733";
 const base_url = "https://v6.exchangerate-api.com/v6/";
 
+fetch(`${base_url}${key}/codes`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    const codes = data.supported_codes;
 
-  fetch(`${base_url}${key}/codes`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      const codes = data.supported_codes;
-
-      let options = "";
-      codes.forEach((code) => {
-        options += `<option value="${code[0]}">${code[0]} - ${code[1]}</option>`;
-      });
-
-      document.querySelector("#cible").innerHTML = options;
-      document.querySelector("#devise").innerHTML = options;
+    let options = "";
+    codes.forEach((code) => {
+      options += `<option value="${code[0]}">${code[0]} - ${code[1]}</option>`;
     });
 
-  async function convert() {
-    const devise = document.querySelector("#devise").value;
-    const montant = document.querySelector("#montant").value;
-    // const target = document.querySelector('#target').value
-    const result = document.querySelector("#result");
+    document.querySelector("#cible").innerHTML = options;
+    document.querySelector("#devise").innerHTML = options;
+  });
 
-    await GET`${base_url}${key}/pair/${devise}/EUR/${target}${montant}`
-      .then((response) => response.json())
-      .then((data) => {
-        result.value = data.conversion_result;
-        innerHTML;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+async function convert() {
+  const devise = document.querySelector("#devise").value;
+  const montant = document.querySelector("#montant").value;
+  const target = document.querySelector("#target").value;
+  const result = document.querySelector("#result");
+
+  await GET`${base_url}${key}/pair/${devise}/EUR/${target}${montant}`
+    .then((response) => response.json())
+    .then((data) => {
+      result.value = data.conversion_result;
+      innerHTML;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 convert();
 
